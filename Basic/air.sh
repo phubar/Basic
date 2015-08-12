7095dfdac5eb0dc37254053fd4c38dd95900ac9f
phubar <phubar@isotomic.com>
2015-08-11 17:52:44 -0700

    add air.sh

---
+++ Basic/air.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# airmon-ng start wlan0
+# airodump-ng -i mon0
+# airodump-ng --bssid <mac of ap> --write <file> mon0 
+# aircrack-ng <file.cap> -w <wordlist>
+# aireplay-ng --deauth 5 -a <mac of ap> -h <host or client optional> mon0 --ignore-negative-one
+
+
