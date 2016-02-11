68c0d105b400773444644a56f9280dbfa74cc0e7
Phu Bar <phubar@isotomic.com>
2016-02-10 17:48:30 -0800

    Update kvm.sh

--- Basic/kvm.sh
+++ Basic/kvm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# create bridge interface
+virt-install --connect qemu:///system -n centos7 --ram 1024 --disk /var/lib/libvirt/images/centos7.img,size=8 --vcpus 1 --os-type linux --os-variant Centos7.0 --graphics none --console pty,target_type=serial --location /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1511.iso --extra-args='console=ttyS0'
+
 
