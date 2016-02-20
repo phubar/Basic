79592e98286d86ebaeb719e1f0b123007b0434ef
Phu Bar <phubar@isotomic.com>
2016-02-20 13:13:33 -0800

    Update kvm.sh

--- Basic/kvm.sh
+++ Basic/kvm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum install kvm virt-manager libvirt virt-install qemu-kvm xauth dejavu-lgc-sans-fonts
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
 virt-install --connect qemu:///system -n centos7 --ram 1024 --disk /var/lib/libvirt/images/centos7.img,size=8 --vcpus 1 --os-type linux --os-variant Centos7.0 --graphics none --console pty,target_type=serial --location /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1511.iso --extra-args='console=ttyS0'
 
 
