42e032fbc2dc936cf8e42b65a7127bb49e6f42a1
Phu Bar <phubar@isotomic.com>
2016-02-21 08:52:19 -0800

    Update kvm.sh

--- Basic/kvm.sh
+++ Basic/kvm.sh
@@ -13,6 +13,7 @@ GATEWAY=
 DNS1=
 # comment out IP on eth0 ; add BRIDGE=br0
 sysctl -p /etc/sysctl.conf
+osinfo-query os # for  list of os-variants
 virt-install --connect qemu:///system -n centos7 --ram 1024 --disk /var/lib/libvirt/images/centos7.img,size=8 --vcpus 1 --os-type linux --os-variant Centos7.0 --graphics none --console pty,target_type=serial --location /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1511.iso --extra-args='console=ttyS0'
 
 
