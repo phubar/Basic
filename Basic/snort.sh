de1345424d1f3cc387efeb1aab55e066362398d4
Phu Bar <phubar@isotomic.com>
2016-10-21 09:56:52 -0700

    Create snort.sh
    initial snort install

---
+++ Basic/snort.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#install prereqs
+yum install epel-release
+yum update
+yum install gcc flex bison zlib zlib-devel libpcap libpcap-devel pcre pcre-devel libdnet libdnet-devel tcpdump
+
+#install daq and snort (from latest for centos)
+yum install https://www.snort.org/downloads/snort/daq-2.0.6-1.centos7.x86_64.rpm
+yum install https://www.snort.org/downloads/snort/snort-2.9.8.3-1.centos7.x86_64.rpm
