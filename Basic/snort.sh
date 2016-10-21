7cfccd8278d070e53f958d95a6de649111220f5a
Phu Bar <phubar@isotomic.com>
2016-10-21 09:59:40 -0700

    add ruleset

--- Basic/snort.sh
+++ Basic/snort.sh
@@ -8,3 +8,6 @@ yum install gcc flex bison zlib zlib-devel libpcap libpcap-devel pcre pcre-devel
 #install daq and snort (from latest for centos)
 yum install https://www.snort.org/downloads/snort/daq-2.0.6-1.centos7.x86_64.rpm
 yum install https://www.snort.org/downloads/snort/snort-2.9.8.3-1.centos7.x86_64.rpm
+
+#install ruleset - download latest from snort.org
+tar -xvzf snortrules-snapshot-2976.tar.gz -C /etc/snort/rules
