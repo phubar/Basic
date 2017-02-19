819acad5bbffdd85d416bb4876d40b68b5b38e2b
Phu Bar <phubar@isotomic.com>
2017-02-19 08:13:25 -0800

    Update mariadb.sh

--- Basic/mariadb.sh
+++ Basic/mariadb.sh
@@ -5,3 +5,9 @@ sudo yum install mariadb-server
 sudo systemctl start mariadb
 mysql_secure_installation
 mysql -u user -p
+
+# example
+# CREATE DATABASE myip;
+# USE myip;
+# create table iptable ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ip VARCHAR(20), ipdate DATE);
+# INSERT INTO iptable (ip,ipdate) VALUES ('0.0.0.0', '1970-01-01');
