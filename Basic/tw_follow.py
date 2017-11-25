39682ff9c28ca1a95f853a06a1dcb184ae042842
Phu Bar <phubar@isotomic.com>
2017-11-25 08:49:13 -0800

    Update tw_follow.py

--- Basic/tw_follow.py
+++ Basic/tw_follow.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 
+# yum install python-devel mysql-devel libmysqlclient-dev
+# pip install mysqlclient
+
 #create table followers ( id INT NOT NULL PRIMARY KEY, following VARCHAR(10), last_update timestamp NOT NULL );
